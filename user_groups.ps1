b543073cd94a3ae4b60883116425f60e5cf5204f
Devon Juvinall <116938619+djuvinall@users.noreply.github.com>
2023-03-09 11:37:23 -0700

    Update user_groups.ps1
    - Added the $user_group_directory variable
    - Made the testing and creation of the $user_group_directory take up less lines
    - Reformatted the try catch so that the catch statement was on the same line as the closing block of the try statement

--- user_groups.ps1
+++ user_groups.ps1
@@ -1,23 +1,17 @@
 $username = Read-Host "Username without domain (i.e. 'jdoe')"
 
 $output_path = "C:\temp\user_groups\{0}_groups.txt" -f $username
+$user_group_dir = "C:\temp\user_groups"
 
-$path_exists = Test-Path "C:\temp\user_groups"
-
-#Create target directory if it does not already exist
-if ($path_exists -eq $false) {
-    mkdir "C:\temp\user_groups"
-}
+if (-not(Test-Path $user_group_dir)) {New-Item -Path $user_group_dir -ItemType Directory}
 
 #Test is user exists, if not, displays a message that the wrong username was typed and re-runs the script
 try {
 get-aduser $username
-}
-catch {
+} catch {
 Write-Host "User does not exist, Please verify Username and try agian"
 Invoke-Expression -Command ($PSCommandPath)
 }
 
 Get-ADPrincipalGroupMembership $username | Select-Object name > $output_path
 Write-host "$username groups sucessgully printed to file"
-Break
