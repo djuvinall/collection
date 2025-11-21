adf5537ef711d2dc305ac15676dd9a3b2beeec84
Devon Juvinall <116938619+djuvinall@users.noreply.github.com>
2025-11-21 15:43:24 -0700

    Switch Statement Error Resolution
    adjusted stop-process to be outside of the Switch statement causing an error

--- SetDarkTheme.ps1
+++ SetDarkTheme.ps1
@@ -1,11 +1,40 @@
-# Set Windows Personalization, color settings, to enable Dark mode for System
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
+# Modify Windows 10/11 or Server theme (Light or Dark mode). Makes registry changes which  
+# take effect upon reboot, or explorer.exe restart. Bypasses restriction to change theme on 
+# unactivated  Windows installations. 
+#
+# Usage:
+#   To switch to dark mode, run: .\theme.ps1 -Mode dark
+#   To switch to light mode, run: .\theme.ps1 -Mode light
+#
+#   Optional: Restart the explorer.exe process: 
+#             "Stop-Process -Name explorer -Force; Start-Sleep -Seconds 2; Start-Process explorer"
 
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("light", "dark")]
+    [string]$Mode
+)
 
-# Set Windows Personalization, color settings, to enable Dark mode for Apps
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
-Write-Host "Dark theme enabled."
+function Set-DarkTheme {
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
+    Write-Host -f Yellow "Dark theme enabled.`n"
+}
 
+function Set-LightTheme {
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 1
+    Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 1
+    Write-Host -f Yellow "Light theme enabled.`n"
+}
+
+switch ($Mode) {
+    "dark" {
+        Set-DarkTheme
+    }
+    "light" {
+        Set-LightTheme
+    }
+}
 
 # Stops and starts the explorer process to update the taskbark color
 Stop-Process -Name explorer -Force; Start-Sleep -Seconds 2; Start-Process explorer
