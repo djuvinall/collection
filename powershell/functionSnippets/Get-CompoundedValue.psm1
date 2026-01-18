6648a3602a5dc0ab7a55a47abf33f965af6d8e4a
Devon Juvinall <116938619+djuvinall@users.noreply.github.com>
2026-01-17 19:18:07 -0800

    Add Get-CompoundedValue function for calculations

---
+++ powershell/functionSnippets/Get-CompoundedValue.psm1
@@ -0,0 +1,19 @@
+function Get-CompoundedValue {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory)]
+    [decimal]$Principal,
+
+    [Parameter(Mandatory)]
+    [decimal]$Rate,   # e.g. 0.05 for 5%
+
+    [Parameter(Mandatory)]
+    [ValidateRange(0, [int]::MaxValue)]
+    [int]$Period
+  )
+
+  $factor = [decimal][math]::Pow([double](1 + $Rate), $Period)
+  $Principal * $factor
+}
+
+Export-ModuleMember -Function Get-CompoundedValue 
