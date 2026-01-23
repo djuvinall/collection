b86a247e826608b4265f02a26cd465a99e8688ae
Devon Juvinall <116938619+djuvinall@users.noreply.github.com>
2026-01-23 15:05:46 -0700

    Add runtime note to Get-AvailableWindowsUpdates.ps1
    Added a note indicating the script's runtime and limitations.

--- powershell/work-in-progress/Get-AvailableWindowsUpdates.ps1
+++ powershell/work-in-progress/Get-AvailableWindowsUpdates.ps1
@@ -1,3 +1,4 @@
 # Get-AvailableWindowsUpdates
+# Takes a minute to run as it searches for updates
 # Will retreive available windows updates but you cannot push them with this method.
 ((New-Object -ComObject Microsoft.Update.Session).CreateUpdateSearcher()).Search("IsInstalled=0").Updates | Format-List Title, IsDownloaded, KBArticleIDs, MsrcSeverity
