b9c065297ba3203d7f5fe08e1dec8122b21c6117
Devon Juvinall <116938619+djuvinall@users.noreply.github.com>
2026-02-05 11:27:08 -0700

    Fixed typo
    Changed 'CMI' to 'CIM' in todo comments

--- powershell/Scripts/Get-DeviceSpecs.ps1
+++ powershell/Scripts/Get-DeviceSpecs.ps1
@@ -34,4 +34,4 @@ Write-Host "Computer specifications collected and saved to $PSScriptRoot\Specs.j
 #### TO DO ####
 # Need to modularize this and turn it into a function
 # It will need to return a rich powershell object
-# Need to convert the WMI calls to CMI calls
+# Need to convert the WMI calls to CIM calls
